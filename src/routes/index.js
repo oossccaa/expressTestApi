8a8a9254750a32eec02c500c02cbb6c37d3744e0
tris.xyh <tris.xyhlink8tech.tw>
2022-05-13 10:22:12 +0800

    init

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import session from './session';
+import user from './user';
+import leave from './leave';
+import attendance from './attendance';
+
+export default {
+  session,
+  user,
+  leave,
+  attendance
+};
